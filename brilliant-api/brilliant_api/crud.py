ec759ce4133b2af4542ec65c1039019087255efd
Sheila-nk <sheilankw@gmail.com>
2024-09-10 15:56:59 +0300

    add API documentation

--- brilliant-api/brilliant_api/crud.py
+++ brilliant-api/brilliant_api/crud.py
@@ -14,14 +14,7 @@ import schemas
 
 def get_brilliant_thing(db: Session):
     # query database for all brilliant thing entries
-    # entries = db.query(models.BrilliantThing).all()
-
     brilliant_thing = db.query(models.BrilliantThing).order_by(func.random()).first()
-
-    # max_record = len(entries)
-    # random_num = randint(1, max_record)
-
-    # brilliant_thing = db.query(models.BrilliantThing).filter(models.BrilliantThing.id==random_num).first()
 
     # return a random brilliant thing
     return brilliant_thing
@@ -29,7 +22,6 @@ def get_brilliant_thing(db: Session):
 
 def create_brilliant_thing(db: Session, brilliant_thing: schemas.BrilliantThingCreate):
     # add a brilliant thing entry to database
-    print(brilliant_thing, type(brilliant_thing))
     new_entry = models.BrilliantThing(entry=brilliant_thing.entry)
 
     db.add(new_entry)
@@ -38,5 +30,5 @@ def create_brilliant_thing(db: Session, brilliant_thing: schemas.BrilliantThingC
     # refresh your instance so that it contains any new data from the database
     db.refresh(new_entry)
 
-    # return the id of the brilliant thing added: "Brilliant thing #123"
+    # return brilliant thing added: "Brilliant thing #123"
     return new_entry
